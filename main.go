d19264595c68df6cc63bf6754927cd2d3c445a40
farhanfatur <farhanrosi2@gmail.com>
2019-08-20 18:10:47 +0700

    the hell

--- main.go
+++ main.go
@@ -7,38 +7,46 @@ import (
 )
 
 func send(c chan<- int, i int, wg *sync.WaitGroup) {
-	fmt.Println("Send a job", i)
+	fmt.Println("send a job", i)
 	c <- i
 	time.Sleep(time.Millisecond * 1000)
 
 	wg.Done()
 }
 
-func check(c <-chan int) {
+func receive(c <-chan int, cReceive chan int) {
 	for {
-		fmt.Println("receive a job", <-c)
+		var num = <-c
+		select {
+		case <-c:
+			fmt.Println("Receive a job", <-c)
+		case cReceive <- <-c:
+			cReceive <- num
+			fmt.Println(<-cReceive)
+		}
 	}
-}
 
-func receive(c <-chan int) {
-
-	for {
-		fmt.Println(<-c)
-	}
 }
 
 func main() {
 	var wg sync.WaitGroup
+	var mtx sync.Mutex
 	c := make(chan int)
+	cReceive := make(chan int)
 	for i := 1; i <= 20; i++ {
 		wg.Add(1)
 		go send(c, i, &wg)
 	}
 	go func() {
-		check(c)
-		wg.Done()
-		close(c)
+		mtx.Lock()
+		receive(c, cReceive)
+		mtx.Unlock()
 	}()
-
+	fmt.Println(cReceive)
+	// go func() {
+	// 	for {
+	// 		fmt.Println("parsingReceive:", cReceive)
+	// 	}
+	// }()
 	wg.Wait()
 }
